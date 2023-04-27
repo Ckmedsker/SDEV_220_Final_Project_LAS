9ead4220720321e8a368bd09b3aa7866fc92e63c
David Amon <damon@ivytech.edu>
2023-04-27 14:20:07 -0400

    Incorporated Tia's HTML pages
    Placed the files into the project where they belong, and updated the views and urls files so they can be accessed through the address bar.

--- inventory/urls.py
+++ inventory/urls.py
@@ -3,6 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.landing_page, name='landing_page'), #root 'landing page/home page'
+    path('products/', views.products_page, name='products_page'),
+    path('sales/', views.sales_page, name='sales_page'),
+    path('dashboard/', views.dashboard_page, name='dashboard_page'), # is this supposed to be admin or landing page?
 
     # these are example templates to help remember what is going on
     # otherPageView1 and the other two are linked to views.py, the names must be changed together
--- inventory/views.py
+++ inventory/views.py
@@ -3,6 +3,15 @@ from django.shortcuts import render
 def landing_page(request):
     return render(request, 'inventory/index.html', {}) #the directory inventory/index.html is grabbing is in the templates folder in the project
 
+def products_page(request):
+    return render(request, 'inventory/products.html', {})
+
+def sales_page(request):
+    return render(request, 'inventory/sales.html', {})
+
+def dashboard_page(request):
+    return render(request, 'inventory/dashboard.html', {})
+
 # these are example templates to help remember what is going on
 # otherPageView1 and the other two are linked to urls.py, the names must be changed together
 
